5228e7abc9307ff3eaf9ce3e8c2f768ae24e7134
Craig <wellspring.craig@gmail.com>
2022-01-31 21:06:16 -0600

    Employee Base Class

---
+++ Lannister-Qwiktrippin/Lannister-Qwiktrippin/Employee.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lannister_Qwiktrippin
+{
+    public enum JobTitle
+    {
+        Associate,
+        AssistantManager,
+        StoreManager,
+        DistrictManager
+    }
+
+    internal class Employee
+    {
+        public string Name { get; private set; }
+        public JobTitle Position { get; private set; }
+        public int StoreNumber { get; private set; }
+        public float Sales { get; private set; }
+
+        public Employee(string _name, JobTitle _position, int _storeNumber, float _sales)
+        {
+            Name = _name;
+            Position = _position;
+            StoreNumber = _storeNumber;
+            Sales = _sales;
+        }
+
+        public void AddSales(float _increase)
+        {
+            Sales += _increase;
+        }
+    }
+}
