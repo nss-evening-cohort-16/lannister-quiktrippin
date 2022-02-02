bb29d8429de90c70abbf2aa7288017ea17900eba
Craig <wellspring.craig@gmail.com>
2022-02-01 19:35:28 -0600

    Store Number Prompt

---
+++ Lannister-Qwiktrippin/Lannister-Qwiktrippin/EnterStoreNumber.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lannister_Qwiktrippin
+{
+    public class EnterStoreNumber
+    {
+        public static int Prompt()
+        {
+            int storeNumber;
+
+            Console.WriteLine("Enter Store Number");
+
+            string? input = Console.ReadLine();
+            while (input == null || !int.TryParse(input, out storeNumber))
+            {
+                Console.WriteLine("Enter a Valid Store Number");
+                input = Console.ReadLine();
+            }
+            return storeNumber;
+        }
+    }
+}
