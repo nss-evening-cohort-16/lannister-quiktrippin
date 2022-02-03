85792c3562b343735fca67b1db68d4a9762d5d55
Craig <wellspring.craig@gmail.com>
2022-02-02 19:24:37 -0600

    Store Merge

--- Lannister-Qwiktrippin/Lannister-Qwiktrippin/District.cs
+++ Lannister-Qwiktrippin/Lannister-Qwiktrippin/District.cs
@@ -9,12 +9,12 @@ namespace Lannister_Qwiktrippin
     {
         public string Name { get; private set; }
         public DistrictManager Manager { get; private set; }
-        //public List<Store> Stores;
+        public List<Store> Stores;
         public District(string _name, string _managerName)
         {
             Name = _name;
             Manager = new DistrictManager(_managerName, _name);
-            //Stores = new List<Store>();
+            Stores = new List<Store>();
         }
     }
 }
--- Lannister-Qwiktrippin/Lannister-Qwiktrippin/Store.cs
+++ Lannister-Qwiktrippin/Lannister-Qwiktrippin/Store.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Lannister_Qwiktrippin
 {
-    class Store
+    public class Store
     {
         public List<Employee> employees;
         public int storeNumber;
