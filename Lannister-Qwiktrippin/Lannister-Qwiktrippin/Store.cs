a4b5e99b869c5ce6ae9ae63bfebf16d3ab9351b3
Messi_G <negash4@gmail.com>
2022-02-02 18:17:16 -0600

    Store class added.

---
+++ Lannister-Qwiktrippin/Lannister-Qwiktrippin/Store.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lannister_Qwiktrippin
+{
+    class Store
+    {
+        List<string> store = new List<string>();
+        public float GasSale { get; set; }
+        public float RetailSale { get; set; }
+        public Store (float gasSale, float retailSale)
+        {
+            GasSale = gasSale;
+            RetailSale = retailSale;
+        }
+    }
+}
